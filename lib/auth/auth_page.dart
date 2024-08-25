8412c9623bcf7f36da15b2e2465708de405f52ca
Neeraj Dwivedi <neeraj.dwivedi@consultant.mannai.com.qa>
2024-08-25 15:45:59 +0300

    adding images and toast msgs

--- lib/auth/auth_page.dart
+++ lib/auth/auth_page.dart
@@ -7,6 +7,7 @@ import 'package:fitnestx_flutter/onboarding/landing.dart';
 import 'package:fitnestx_flutter/registration/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:hive/hive.dart';
 
 class AuthPage extends StatefulWidget {
@@ -25,23 +26,18 @@ class _AuthPageState extends State<AuthPage> {
   void initState() {
     super.initState();
     initConnectivity();
-
     connectivitySubscription =
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
 
   Future<void> initConnectivity() async {
     late List<ConnectivityResult> result;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       result = await _connectivity.checkConnectivity();
     } on PlatformException catch (e) {
       return;
+      //showToast("Internet connectivity not available.${e.message}");
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) {
       return Future.value(null);
     }
@@ -53,8 +49,6 @@ class _AuthPageState extends State<AuthPage> {
     setState(() {
       _connectionStatus = result;
     });
-    // ignore: avoid_print
-    print('Connectivity changed: $_connectionStatus');
   }
 
   @override
@@ -72,7 +66,6 @@ class _AuthPageState extends State<AuthPage> {
             }
             return const WelcomePage();
           }
-          print(_connectionStatus);
           if (_connectionStatus.contains(ConnectivityResult.none)) {
             return const Savedpage();
           } else {
